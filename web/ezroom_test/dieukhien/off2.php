d842eecb64a02a7a0731c4e1b3d680cf1e2b6e73
Dat Nguyen <90817736+Futa612@users.noreply.github.com>
2021-09-25 23:08:40 +0700

    up public_html tu 000webhost
    thi xoa cai nay

--- web/ezroom_test/dieukhien/off2.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $ramdom_file = fopen("data.txt", "w");
-    $str = "20";
-    fwrite($ramdom_file, $str);
-    fclose($ramdom_file);
-    // header("Location: ezroom/bangdieukhien.php");
-?>
--- web/ezroom_test/dieukhien/on4.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $ramdom_file = fopen("data.txt", "w");
-    $str = "41";
-    fwrite($ramdom_file, $str);
-    fclose($ramdom_file);
-//     header("Location: ezroom/bangdieukhien.php");
-?>
